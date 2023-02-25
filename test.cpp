c9ffe8ac36fa9557ba87763c31fba0ca7a6fc7b0
silverwind <me@silverwind.io>
2023-02-25 15:58:11 +0100

    add test.cpp

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <stream>
+
+using namespace std;
+
+int main() {
+    int foo;
+    float bar;
+    cout << "string";
+    while (false) {
+        cin >> foo >> bar;
+    }
+    return 0;
+}
