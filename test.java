ec1d64b2f2affe773761bfdd19c8189731e7968f
silverwind <me@silverwind.io>
2023-09-08 17:39:17 +0200

    add test.java

---
+++ test.java
@@ -0,0 +1,17 @@
+class Bicycle {
+
+  // field of class
+  int gear = 5;
+
+  // method of class
+  void braking() {
+    ...
+  }
+}
+
+// create object
+Bicycle sportsBicycle = new Bicycle();
+
+// access field and method
+sportsBicycle.gear;
+sportsBicycle.braking();
