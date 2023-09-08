43c408103bedc9760939001b5e43d40022e37d6a
silverwind <me@silverwind.io>
2023-09-08 17:38:24 +0200

    class

--- test.jsx
+++ test.jsx
@@ -8,6 +8,10 @@ function createClassNameString(classNames) {
   return classNames.join(' ');
 }
 
+class foo {
+  bar: () => {}
+}
+
 // this comment is here to demonstrate an extremely long line length, well beyond what you should probably allow in your own code, though sometimes you'll be highlighting code you can't refactor, which is unfortunate but should be handled gracefully
 
 function createChildren(style, useInlineStyles) {
